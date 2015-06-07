88cca66ba5347241bd568765c9ca79ed6a31c1b6
Brian Bosak <webadm@elcnet.servehttp.com>
2015-06-07 15:20:07 -0500

    Added missing files

---
+++ GGRouter.h
@@ -0,0 +1,12 @@
+#ifndef GGRouter
+#define GGRouter
+extern "C" {
+  void Platform_Free(void* obj);
+  void* Platform_Open_Named_Channel(const char* name);
+  void* Platform_Channel_Connect(const char* name);
+  void Platform_Channel_Transmit(void* channel, void* data, size_t len);
+  void Platform_Channel_Receive(void* channel, void* thisptr, void(*callback)(void*,void*,size_t));
+  void Platform_Channel_ReadMsg(void* channel, void* thisptr, void(*callback)(void*,void*,void*,size_t));
+  
+}
+#endif
