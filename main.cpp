0bd406a94a6059987c4a16aae17d65713376a16a
Brian Bosak <webadm@elcnet.servehttp.com>
2015-10-01 14:46:40 -0500

    Misc. changes

--- main.cpp
+++ main.cpp
@@ -280,7 +280,8 @@ int main(int argc, char** argv) {
       std::string qres = DotQuery(domain.c_str(),&resolved);
       if(qres.empty()) {
 	printf("ERROR: Unable to resolve domain authority chain -- invalid Internet or malformed name. If you want to make an Internet, please run the makeinet command prior to assigning a domain name.\n");
-	printf("FATAL ERROR. HALT (with segfault)\n");
+	printf("FATAL ERROR.\n");
+	sleep(-1);
 	return -1;
       }
       printf("Query for domain registration returned: %s\n",qres.data());
