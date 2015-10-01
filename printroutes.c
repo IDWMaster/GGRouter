fe46476c25455f7a2c61141a9b03aa9456817f3b
Brian Bosak <webadm@elcnet.servehttp.com>
2015-10-01 13:02:25 -0500

    Fixed

--- printroutes.c
+++ printroutes.c
@@ -13,7 +13,8 @@ size_t i;
 for(i = 0;i<count;i++) {
 uint64_t val;
 read(STDIN_FILENO,&val,8);
-printf("%i.%i.%i.%i:%i\n",(int)(unsigned char)(val),(int)(unsigned char)(val << 8),(int)(unsigned char)(val << 16),(int)(unsigned char)(val << 24),(int)(val << 32));
+//printf("%i.%i.%i.%i:%i\n",(int)(unsigned char)(val),(int)(unsigned char)(val << 8),(int)(unsigned char)(val << 16),(int)(unsigned char)(val << 24),(int)(val << 32));
+printf("%i\n",(int)(val << 32));
 }
 
 return 0;
